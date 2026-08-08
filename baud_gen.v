8b587aa87b7765c86c3f6186ab0b3fce57074890
Ishaan Trivedi <137341691+Coderguy71@users.noreply.github.com>
2026-08-08 11:12:00 -0400

    Update baud_gen.v
    initial baud_gen finished code

--- baud_gen.v
+++ baud_gen.v
@@ -9,14 +9,20 @@ module baud_rate_gen#(
 );
 
 localparam Baud_val = ((CLOCKVALUE)/(BAUDRATE * OVERSAMPLE)) - 1; //local param so noone can accidently assign baud_val 
+reg[11:0] count; //used $clog2(2812)=12 since thats the largest value needed to be held. This is wasteful rightnow since rx doesn't need that much (175) *TO BE CHANGED*
 
+always @(posedge clk) begin
+    baud_clock <= 0;
 
-
-
+    if(reset)
+        count <= 0;
+    else if (Baud_val == count) begin
+        baud_clock <= 1;
+        count <= 0;
+    end
+    else
+        count <= count+1;
+end
 
 
 endmodule
-
-
-
-
