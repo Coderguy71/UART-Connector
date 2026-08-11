869dd8b8d44e9cad540c971c58bca1ddf31df2b2
Ishaan Trivedi <137341691+Coderguy71@users.noreply.github.com>
2026-08-10 20:28:17 -0400

    Update ALU_Testbench.v
    Updated ALU testbench for readability (also ran in EDA Playground)

--- ALU_Testbench.v
+++ ALU_Testbench.v
@@ -12,31 +12,52 @@ module ALU_Testbench;
     );
 
     initial begin
+      //originally used: $display("A=%d B=%d Op=%b Result=%d", A, B, Operation, result); //%d means decimal and %b mean binary
         assign A = 4'b0001;
         assign B = 4'b0001;
         assign Operation = 3'b001;
          #10;
-      	 $display("A=%d B=%d Op=%b Result=%d", A, B, Operation, result); //%d means decimal and %b mean binary
+         if (result == 8'd2)
+            $display("ADD: PASS (result=%d)", result);
+        else
+            $display("ADD: FAIL (result=%d)", result);
+        
         assign A = 4'b0001;
         assign B = 4'b0001;
         assign Operation = 3'b010;
          #10;
-      	 $display("A=%d B=%d Op=%b Result=%d", A, B, Operation, result);
+         if (result == 8'd0)
+            $display("SUB: PASS (result=%d)", result);
+        else
+            $display("SUB: FAIL (result=%d)", result);
+
         assign A = 4'b0001;
         assign B = 4'b0001;
         assign Operation = 3'b011;
          #10;
-      	 $display("A=%d B=%d Op=%b Result=%d", A, B, Operation, result);
+         if (result == 8'd1)
+            $display("MULT: PASS (result=%d)", result);
+        else
+            $display("MULT: FAIL (result=%d)", result);
+        
         assign A = 4'b0001;
         assign B = 4'b0001;
         assign Operation = 3'b100;
          #10;
-      	 $display("A=%d B=%d Op=%b Result=%d", A, B, Operation, result);
+         if (result == 8'd2)
+            $display("SHIFT LEFT: PASS (result=%d)", result);
+        else
+            $display("SHIFT LEFT: FAIL (result=%d)", result);
+
         assign A = 4'b0001;
         assign B = 4'b0001;
         assign Operation = 3'b101;
          #10;
-      	 $display("A=%d B=%d Op=%b Result=%d", A, B, Operation, result);  
+         if (result == 8'd0)
+            $display("SHIFT RIGHT: PASS (result=%d)", result);
+        else
+            $display("SHIFT RIGHT: FAIL (result=%d)", result);
+
         $finish;
     end
 
