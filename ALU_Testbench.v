e420d5b5eeffd2f5819166cf207bffb6165bb131
Ishaan Trivedi <137341691+Coderguy71@users.noreply.github.com>
2026-08-11 15:18:46 -0400

    Update ALU_Testbench.v
    Finished ALU_Testbench

--- ALU_Testbench.v
+++ ALU_Testbench.v
@@ -26,9 +26,9 @@ module ALU_Testbench;
             test_count = test_count + 1;
             if(result == Expected_result) begin
                 pass_count = pass_count + 1;
-                $display("%s: PASS (result=%d)", test_name, result);
+                $display("%s: PASS (result=%0d)", test_name, result);
             end else begin
-                $display("%s: FAIL (expected=%d, result=%d)", test_name, Expected_result, result);
+                $display("%s: FAIL (expected=%d, result=%0d)", test_name, Expected_result, result);
             end
         end
     endtask
@@ -43,10 +43,13 @@ module ALU_Testbench;
         run_test(4'd1, 4'd1, 3'b100, 8'd2, "SHIFT_LEFT");
         run_test(4'd1, 4'd1, 3'b101, 8'd0, "SHIFT_RIGHT");
         run_test(4'd1, 4'd1, 3'b111, 8'd0, "OP_ERROR");
-        run_test(4'd1, 4'd1, 3'b111, 8'd0, "OP_ERROR");
+        run_test(4'd0, 4'd1, 3'b010, 8'd255, "SUB_OVERFLOW");
+        run_test(4'd15, 4'd15, 3'b011, 8'd225, "MULT_MAX");
+        run_test(4'd15, 4'd4, 3'b100, 8'd240, "SHIFTL_OVER");
+        run_test(4'd15, 4'd2, 3'b101, 8'd3, "SHIFTR_TEST2");
 
         $display("---");
-        $display("%d/%d tests passed", pass_count, test_count);
+        $display("%0d/%0d tests passed", pass_count, test_count);
         $finish;
     end
 
