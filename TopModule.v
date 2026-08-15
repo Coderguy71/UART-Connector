c16f2adce31bf3cee3553e0247d58e9cfa1f40f9
Ishaan Trivedi <137341691+IshaanTrived1@users.noreply.github.com>
2026-08-15 11:49:29 -0400

    Create TopModule.v

---
+++ TopModule.v
@@ -0,0 +1,38 @@
+module TopModule(
+    input reset, clk, rx_in,
+    output wire tx
+);
+
+    wire busy, ALUready;
+    wire[2:0] opcode;
+    wire[3:0] A, B;
+    wire[7:0] result;
+
+    CommandParser topCommandParser(
+        .reset(reset),
+        .clk(clk),
+        .rx_in(rx_in),
+        .opcode(opcode),
+        .A(A),
+        .B(B),
+        .ALUready(ALUready)
+    );
+
+
+    ALU TopALU(
+        .A(A),
+        .B(B),
+        .Operation(opcode),
+        .result(result)
+    );
+
+        TX TopTX(
+        .clk(clk),
+        .reset(reset),
+        .data_in(result),
+        .start(ALUready),
+        .tx(tx),
+        .busy(busy)
+    );
+
+endmodule
