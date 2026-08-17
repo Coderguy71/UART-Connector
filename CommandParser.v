591890e07a6a9c813c27a2b90c4bce4b6bde50b1
Ishaan Trivedi <137341691+IshaanTrived1@users.noreply.github.com>
2026-08-17 09:24:17 -0400

    Update CommandParser.v

--- CommandParser.v
+++ CommandParser.v
@@ -1,16 +1,13 @@
 module CommandParser(
-    input reset, clk, rx_in;
-    input data_done;
-    output reg [2:0] opcode;
-    output reg [3:0] A, B;
-    output reg ALUready;
+    input reset, clk, rx_in,
+    input data_done,
+    output reg [2:0] opcode,
+    output reg [3:0] A, B,
+    output reg ALUready
 );
 
-endmodule
-
 wire [7:0] data_out;
-wire data_done;
-reg[1:0] counter;
+reg [1:0] counter;
 
 RX rx_parser(
     .clk(clk),
@@ -18,35 +15,37 @@ RX rx_parser(
     .rx_in(rx_in),
     .data_out(data_out),
     .data_done(data_done)
-)
+);
 
 always @(posedge clk) begin
     if(reset) begin
-        data_out <= 0;
         A <= 0;
         B <= 0;
+        opcode <= 0;
         ALUready <= 0;
-        data_done <= 0;
         counter <= 0;
     end
     else begin
         ALUready <= 0;
         if(data_done) begin
-            counter <= counter + 1;
-            case(counter) begin
-                2'b01: opcode <= data_out[2:0];
-                2'b10: A <= data_out[3:0];
-                2'b11: begin
-                     B <= data_out[3:0];
-                     ALUready <= 1;
+            case(counter)
+                2'b00: begin
+                    opcode  <= data_out[2:0];
+                    counter <= counter + 1;
                 end
-            end
+                2'b01: begin
+                    A       <= data_out[3:0];
+                    counter <= counter + 1;
+                end
+                2'b10: begin
+                    B        <= data_out[3:0];
+                    ALUready <= 1;
+                    counter  <= 0;
+                end
+                default: counter <= 0;
             endcase
         end
     end
 end
 
-
-
-
-
+endmodule
